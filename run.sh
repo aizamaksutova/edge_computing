39c1cbfb2b3c6721239438303fee72fe01b02c33
Aiza Maksutova <71812108+aizamaksutova@users.noreply.github.com>
2023-09-20 00:37:00 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+TransferSize=32000000
+SpeedTestTime=100
+array=(0 5 10 15 20 25 30)
+./ModifyArg.sh $TransferSize
+for i in "${array[@]}"; do
+  echo "$i "
+  sleep 1
+  ./killStressor.sh
+  for ((j = 1; j <= i; ++j)); do
+    echo "$j Time"
+    ./StressorScript.sh >/dev/null &
+  done
+  sleep 5
+  ./try
+  mv inference_timings.txt $i.txt
+  ./killStressor.sh
+done
+mkdir data
+mv *.txt data/
